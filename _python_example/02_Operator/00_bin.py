740fa1bc6c57ce6b6cb617bd225ebdfac4a196fb
aloHyomora <alohomoratolit@naver.com>
2024-10-16 18:52:17 +0900

    [Add] : problem 1 ~ 3

--- _python_example/02_Operator/00_bin.py
+++ _python_example/02_Operator/00_bin.py
@@ -1,14 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import math # 수학 계산 라이브러리
+
 class ExampleClass:
     def __init__(self):
         self.programs = [
             "프로그램 1. [2진수를 input으로 받아 10진수와 16진수 출력]",
-            "프로그램 2: 파이썬에서 리스트를 어떻게 초기화합니까?",
-            "프로그램 3: 클래스와 객체의 차이를 설명하시오.",
-            "프로그램 4: 리스트와 튜플의 차이는 무엇인가?",
-            "프로그램 5: 파이썬에서 예외 처리를 어떻게 합니까?",
-            "프로그램 6: 상속이란 무엇이며, 어떻게 사용합니까?"
+            "프로그램 2: [반경 r을 입력 받아 구의 표면적과 체적을 출력]",
+            "프로그램 3: [직각삼각형의 밑변과 높이를 입력 받아서 빗변과 밑변과 빗변의 사이각을 구함. 단, 사이각은 °로 출력]",
+            "프로그램 4: [임의의 정수를 입력받아 2진수로 변환했을 때 1인 비트 자리수를 출력. 비트 연산자 << 로 마스크를 만들기]",
+            "프로그램 5: [2000부터 3000까지의 정수 중 모든 자리수의 합이 7이 되는 수들을 모두 출력]",
+            "프로그램 6: [통신 오류를 감지하기 위하여 데이터의 모든 문자의 아스키 코드에 대하여 비트 배타적 논리합 (xor)을 구하여 맨 뒤에 16진수로 2개의 문자를 붙여 전송한다. 이를 BCC(block check code)라 부른다. 수신된 데이터의 BCC를 계산해보고 마지막에 수신한 BCC와 일치하지 않으면 오류인 것이다. 여기서는 수신한 데이터가 Hello python!이라고 가정하고 그 BCC를 구하는 프로그램을 작성해보자.]"
+            "프로그램 7: [[turtle]  관람차 그리기]"
         ]
+    def program_1(self):
+        print()
+        print("프로그램 1을 시작합니다...")
+        inputData = input('2진수를 입력하세요.')
+        print(int(inputData, base=2)) # 2진수를 10진수 출력
+        print(hex(int(inputData, base=2))[2:]) # 2진수를 16진수로 출력
     
+    def program_2(self):
+        print()
+        print("프로그램 2을 시작합니다...")
+        radius = float(input('반경 r을 입력하세요:'))
+        print(f"표면적은 4 x pi x r^2 = {4 * math.pi * math.pow(radius, 2)} 체적은 4 / 3 * pi x r^3 = {4 / 3  * math.pi * math.pow(radius, 3)}")
+                
+    def program_3(self):
+        print()
+        print("프로그램 3을 시작합니다...")
+        width = float(input('밑변을 입력하세요:'))
+        height = float(input('높이를 입력하세요:'))
+        
+        hypotenuse = math.sqrt(height**2 + width**2)
+        seta = math.atan(height/width) * 180 / math.pi
+        
+        print(f'삼각형의 빗변은 {hypotenuse:.4f}이고, 삼각형의 사이각은 {seta:.4f}이다.')
+        
+    def program_4(self):
+        print("프로그램 4~")
+    
+    def program_5(self):
+        print("프로그램 5~")
+        
+    def program_6(self):
+        print("프로그램 6~") 
+    
+    def program_6(self):
+        print("프로그램 6~") 
+        
     def call_method(self, method_string):
         if method_string.strip().startswith("program"):
             try:
@@ -41,23 +81,7 @@ class ExampleClass:
         else:
             print(f"유효하지 않은 번호입니다. 1에서 {self.get_program_count()} 사이의 숫자를 입력하세요.")
     
-    def program_1(self):
-        print("프로그램 1~")
     
-    def program_2(self):
-        print("프로그램 2~")
-    
-    def program_3(self):
-        print("프로그램 3~")
-    
-    def program_4(self):
-        print("프로그램 4~")
-    
-    def program_5(self):
-        print("프로그램 5~")
-        
-    def program_6(self):
-        print("프로그램 6~")
 
 # 클래스 인스턴스 생성
 example = ExampleClass()
@@ -66,10 +90,10 @@ while True:
     try:
         count = example.get_program_count()  # 프로그램 개수 가져오기
         user_input = int(input(f"1~{count} 사이의 번호를 입력하세요. (종료는 0): "))  # 프로그램 개수 출력
-        example.show_all_program()  # 프로그램 목록 출력
         if user_input == 0:
             print("프로그램을 종료합니다.")
             break
+        example.show_all_program()  # 프로그램 목록 출력
         example.call_method(f"program_{user_input}")  # 동적 메서드 호출
     except ValueError:
         print("숫자를 입력하세요.")
